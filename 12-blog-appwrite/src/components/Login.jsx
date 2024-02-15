117d1225a415312beeeca4dc08a71b9c8853d490
nitin729 <redruby@tuta.io>
2024-02-15 14:31:39 +0530

    Fixed login page css and changed the title to 'Blog'

--- 12-blog-appwrite/src/components/Login.jsx
+++ 12-blog-appwrite/src/components/Login.jsx
@@ -30,11 +30,15 @@ const Login = () => {
 
   return (
     <>
-      <div className="flex items-center justify-center w-full">
-        <div className="mb-2 flex justify-center">
-          <span className="inline-block w-full max-w-[100px]">
-            <Logo width="100%" />
-          </span>
+      <div className="flex items-center justify-center m-4">
+        <div
+          className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
+        >
+          <div className="mb-2 flex justify-center">
+            <span className="inline-block w-full max-w-[100px]">
+              <Logo width="100%" />
+            </span>
+          </div>
           <h2 className="text-center text-2xl font-bold leading-tight">
             Sign in to your account
           </h2>
@@ -44,7 +48,7 @@ const Login = () => {
               to="/signup"
               className="font-medium text-primary transition-all duration-200 hover:underline"
             >
-              Sign Up
+              <b>Sign Up</b>
             </Link>
           </p>
           {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
